1dbbe19d79fe086455e58c386487687a7a9eace5
Oded Brilon <oded.brilon@servicenow.com>
2018-06-20 16:58:39 -0700

    Add Access Token

--- gopee.go
+++ gopee.go
@@ -296,13 +296,15 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 				templates.ExecuteTemplate(w, "home.html", nil)
 			} else {
 				log.Println("Invalid Key Provided")
+				http.Redirect(w, r,"/error.html", 302)
 				return
 			}
 		} else {
 			log.Println("No Access Key Provided")
+			http.Redirect(w, r,"/error.html", 302)
 			return
 		}
-		templates.ExecuteTemplate(w, "error.html", nil)
+
 	} else {
 		ProxyRequest(r, w)
 	}
